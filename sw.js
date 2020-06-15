d965f1c78eef2edcbbf08a46f4eb7f9ad703ddf6
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-06-15 22:23:40 +0100

    remove deleted css from service worker

--- sw.js
+++ sw.js
@@ -22,10 +22,6 @@ const precacheFiles = [
 	"/home/images/page/browsers/safari.png",
 	"/home/images/page/browsers/tor.png",
 
-	"/css/theme/auto.css",
-	"/css/theme/black.css",
-	"/css/theme/dark.css",
-	"/css/theme/light.css",
 	"/css/main.css",
 	"/css/settings.css",
 	"/scripts/navbar.js",
@@ -34,48 +30,48 @@ const precacheFiles = [
 	"/fonts/FiraSansNF.woff2",
 ];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
 	self.skipWaiting();
 	event.waitUntil(
-		caches.open(CACHE).then(function (cache) {
+		caches.open(CACHE).then(function(cache) {
 			return cache.addAll(precacheFiles);
-		})
+		}),
 	);
 });
 
 // Allow sw to control of current page
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", function(event) {
 	event.waitUntil(self.clients.claim());
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function(event) {
 	if (event.request.method !== "GET") return;
 	event.respondWith(
 		fromCache(event.request).then(
-			function (response) {
+			function(response) {
 				// The response was found in the cache so we respond with it and update the entry
 				// This is where we call the server to get the newest version of the
 				// file to use the next time we show view
 				event.waitUntil(
-					fetch(event.request).then(function (response) {
+					fetch(event.request).then(function(response) {
 						return updateCache(event.request, response);
-					})
+					}),
 				);
 				return response;
 			},
-			function () {
+			function() {
 				// The response was not found in the cache so we look for it on the server
 				return fetch(event.request)
-					.then(function (response) {
+					.then(function(response) {
 						// If request was success, add or update it in the cache
 						event.waitUntil(updateCache(event.request, response.clone()));
 						return response;
 					})
-					.catch(function (error) {
+					.catch(function(error) {
 					});
-			}
-		)
+			},
+		),
 	);
 });
 
@@ -83,8 +79,8 @@ function fromCache(request) {
 	// Check to see if you have it in the cache
 	// Return response
 	// If not in the cache, then return
-	return caches.open(CACHE).then(function (cache) {
-		return cache.match(request).then(function (matching) {
+	return caches.open(CACHE).then(function(cache) {
+		return cache.match(request).then(function(matching) {
 			if (!matching || matching.status === 404) {
 				return Promise.reject("no-match");
 			}
@@ -94,7 +90,7 @@ function fromCache(request) {
 }
 
 function updateCache(request, response) {
-	return caches.open(CACHE).then(function (cache) {
+	return caches.open(CACHE).then(function(cache) {
 		return cache.put(request, response);
 	});
 }
